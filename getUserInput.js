64bcf9771dc1a3f3695b1c13a43158fb9f034979
JosephIgbaji <igbajijoseph.k@gmail.com>
2023-09-28 10:36:05 +0100

    updated read user input file

--- getUserInput.js
+++ getUserInput.js
@@ -4,9 +4,14 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.question("How are are you: ", (age) => {
+rl.question("How old are are you: ", (age) => {
   age >= 18
     ? console.log("You are qualified")
     : console.log("You below privilege");
   rl.close();
+});
+
+rl.on("close", () => {
+  console.log("Input Interface Closed");
+  process.exit(0);
 });
